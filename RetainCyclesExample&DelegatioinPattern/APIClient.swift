5d9a9a04017e3edfd474aa40e87978f0ba195ba2
Fernando Negrete Pimentel <fnegrete9510@gmail.com>
2023-12-26 11:28:19 -0600

    Set Api Client

---
+++ RetainCyclesExample&DelegatioinPattern/APIClient.swift
@@ -0,0 +1,39 @@
+//
+//  APIClient.swift
+//  RetainCyclesExample&DelegatioinPattern
+//
+//  Created by Fernando Negrete Pimentel on 26/12/23.
+//
+
+import Foundation
+
+struct PokemonDataModel: Decodable{
+    let name : String
+    let url: String
+}
+
+struct PokemonResponseDataModel: Decodable {
+    let Pokemons: [PokemonDataModel]
+    
+    enum  CodingKeys: String, CodingKey {
+        case results
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.Pokemons = try container.decode([PokemonDataModel].self, forKey: .results)
+    }
+}
+
+class APICLient {
+    func getPokemon(){
+        
+        let url = URL(string: "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=151")!
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            let dataModel = try! JSONDecoder().decode(PokemonResponseDataModel.self, from: data!)
+            print("DataModel \(dataModel)")
+        }
+        task.resume()
+    }
+}
--- RetainCyclesExample&DelegatioinPattern/ViewController.swift
+++ RetainCyclesExample&DelegatioinPattern/ViewController.swift
@@ -8,10 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let apiClient = APICLient()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 2
+        label.textAlignment = .center
+        label.text = "Prueba1"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var searchButton: UIButton = {
+        var configuration = UIButton.Configuration.bordered()
+        configuration.title = "Busca un pokemon"
+        
+        let button = UIButton(type: .system, primaryAction: UIAction(handler:{ [weak self] _ in
+            self?.didTapOnAcceptButton()
+        }))
+        
+        button.configuration = configuration
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(searchButton)
+        view.addSubview(titleLabel)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.bottomAnchor.constraint(equalTo: searchButton.topAnchor, constant: -32),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: view.trailingAnchor),
+            searchButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            searchButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+        ])
+    }
+    
+    func didTapOnAcceptButton(){
+        apiClient.getPokemon()
     }
 
 
